fa40c090c59874abd3144b6786aa4cd2763e5846
JNCamil <juancamvh@hotmail.com>
2023-06-02 14:15:47 +0200

    estructura1

---
+++ includes/header.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./public/css/bootstrap/bootstrap.min.css">
+    <link rel="stylesheet" href="./public/css/estyles.css">
+    <link rel="stylesheet" href="./public/fontawesome-free-6.4.0-web/css/all.css"> <!-- A FALTA DE VER EL JS Y LAS INTERACCIONES CON LOS ICONOES -->
+    <script src="./public/js/bootstrap/bootstrap.bundle.min.js"></script>
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
